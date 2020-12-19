bc23be65ea2dc75426d31f9d2e5053badce5bf6a
Bartosz Daniszewski <bdaniszewski@multic.pl>
2020-12-19 10:47:40 +0100

    Show the calories of a product and display the number of results found in FoodResults

--- src/components/FoodResults.js
+++ src/components/FoodResults.js
@@ -4,6 +4,7 @@ const FoodResults = (props) => {
   const errorMessage = null;
   return (
     <div>
+      <p>Found {props.results.length} results</p>
       <ul>
         {props.results.map((resultItem) => (
           <li key={resultItem.nix_item_id}>
@@ -11,6 +12,7 @@ const FoodResults = (props) => {
               <img src={resultItem.photo.thumb} />
               <figcaption>{resultItem.brand_name_item_name}</figcaption>
             </figure>
+            <span>{resultItem.nf_calories} calories</span>
           </li>
         ))}
       </ul>
